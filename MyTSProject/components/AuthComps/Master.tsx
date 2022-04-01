ef37c18e26198f676e779b12e56688e1328dbf20
priyanshiisingh <priyanshianamika1815@gmail.com>
2022-04-01 15:02:01 +0530

    changes

--- MyTSProject/components/AuthComps/Master.tsx
+++ MyTSProject/components/AuthComps/Master.tsx
@@ -9,11 +9,40 @@ import {
 } from "react-native";
 import Login from "./Login";
 import SignUp from "./SignUp";
+import { signInWithRedirect } from "firebase/auth";
+import { auth, provider } from "../../database/Firestore";
+// import GoogleAuth from "../GoogleAuth";
 
 export default function Master({ navigation }) {
   const image = {
     uri: "https://images.unsplash.com/photo-1564352969906-8b7f46ba4b8b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
   };
+
+  function GoogleAuth() {
+    signInWithRedirect(auth, provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = provider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        Alert.alert("User Logged In!");
+
+        navigation.navigate("Drawer");
+        // ...
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.email;
+        // The AuthCredential type that was used.
+        const credential = provider.credentialFromError(error);
+        // ...
+      });
+  }
+
   return (
     <ImageBackground source={image} resizeMode="cover" style={styles.image}>
       <Text style={styles.text}>Welcome!</Text>
